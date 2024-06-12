ca375bc3d63e34d04b576995c13907005054a668
thangasujithas <144225567+thangasujithaacs@users.noreply.github.com>
2024-06-12 17:59:35 +0530

    Update main.js

--- main/src/main.js
+++ main/src/main.js
@@ -12,24 +12,6 @@ export default async ({ res, req, log, error }) => {
   ]);
   const appwrite = new AppwriteService();
   console.log(req.method)
-  if (req.method === 'GET'){
-
-    const shortId = req.path.replace(/^(\/)|(\/)$/g, '');
-    log(`Fetching document with ID: ${shortId}`);
-
-    const urlEntry = await appwrite.getURLEntry(shortId);
-
-    if (!urlEntry) {
-      return res.send('Invalid link.', 404);
-    }
-
-    if (urlEntry.expirationDate && new Date() > new Date(urlEntry.expirationDate)) {
-      return res.send('This link has expired.', 410); // 410 - Gone status code indicates the resource is no longer available
-    }
-
-    return res.redirect(urlEntry.url);
-  }
-  else{
     try {
       throwIfMissing(req.body, ['url']);
       new URL(req.body.url);
@@ -65,7 +47,6 @@ export default async ({ res, req, log, error }) => {
       error(err.message);
       return res.send({ success: false, error: err.message }, 400);
     }
-  
-  }
+    
     
 };
